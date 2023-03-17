e82cceaced21f11cd0d7d30543b0f44ca0fa299c
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-16 17:58:44 -0700

    too spacific

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
  * main - prints Programming is like building a multilingual puzzle
  *
  * Return : Always 0
- *
  */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	Return (0);
 	 
 }
