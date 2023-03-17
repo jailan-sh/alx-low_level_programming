33e14f05da9a00d44747188fc8e16a4b6f2277ce
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-16 19:27:16 -0700

    hard

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
- * main -  Size is not grandeur, and territory does not make a nation
- *
- * Return: Always 0 (sucess)
+ * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
-	{
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
