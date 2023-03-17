2ab4154c3ab0e3a54974a6fb805bb68721ae44be
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-17 13:19:32 -0700

    positive task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 #include <stdio.h>
-
 /**
- * main	-this variable task
+ * main	-this positive or negative file
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
-
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if	(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is zero", n);
-
+		printf("%d is zero\n", n);
 	return (0);
 }
