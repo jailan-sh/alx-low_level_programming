20190ec4ef99f704322b3f4c39ff54cdfeab6870
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-17 12:47:40 -0700

    posirive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main	-this variable task
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if	(n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero", n);
+
+	return (0);
