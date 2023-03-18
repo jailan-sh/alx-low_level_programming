4d9ab635db210cd33671b4a305a5bd63dd714ee5
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-17 18:14:04 -0700

    alphapitic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main	-coding
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+		return (0);
+}
