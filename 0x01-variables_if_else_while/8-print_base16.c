4d2b36f59bb816b6ac346ec42fc7eadd4a626ddf
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-18 06:10:34 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main	-coding
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int g;
+
+	for (i = 0; i >= 9; i++)
+	{
+		putchar('i');
+	}
+	for (i = 'a'; g <= 'f'; g++)
+	{
+		putchar('g');
+	}
+	putchar ('\n');
+	return (0);
+}
+
