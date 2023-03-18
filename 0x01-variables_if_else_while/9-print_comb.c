07d7545ad384c04578157f92eea8c0f7a41e46e6
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-18 12:39:36 -0700

    codding

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  * main	-coding
- * Return: 0
+ *
+ * Return:0
  */
+
 int main(void)
 {
 	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(48 + num);
+		putchar(num + '0');
 	}
 	putchar (',');
+	putchar (' ');
 	return (0);
 }
-
