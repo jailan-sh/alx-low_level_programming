7af71ffec97b8a3a6f0b745b35c712939043c533
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-18 04:42:19 -0700

    alphabitic

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main	-coding
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch = 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
+
