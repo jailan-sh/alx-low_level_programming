7d63ff8bc83646d56238831b369a900452b8d8dc
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-19 16:39:18 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+ /**
+  * main -coding
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 1; b < 9; b++)
+		{
+			for (c = 2; c < 10; c++)
+			{
+				if (a < c && b < c && a < b && a != c && c != b && a != b)
+				{
+				putchar(a + '0');
+			putchar(b + '0');
+		putchar(c + '0');
+
+			if (a < 6)
+			{
+			putchar(',');
+		putchar(' ');
+			}
+				}
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
