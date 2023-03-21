6f829bb0a0cad7e155a0d559c469107c0f7e3f74
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-21 16:58:20 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -coding
+ *
+ * Return: 0
+ */
+int _islower(int c);
+int main(void)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		_putchar(0);
+	}
+	else
+	{
+		_putchar(1);
+	}
+	return (0);
+}
