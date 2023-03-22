c01140c685aa8ab3b5c972c8f654b966b9ab7988
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-21 18:10:04 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+	if (ab >= '-1')
+	{
+		_putchar(ab * '-1');
+	}
+	else
+		(ab >= '1')
+	{
+		_putchar(ab);
+	}
+}
