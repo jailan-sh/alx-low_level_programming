1287ae193ba7a2ffb0b01ec2ca0f3aed5c244b05
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-22 05:31:23 -0700

    task10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * add - a function that adds two integers and returns the result
- * @one: first number input
- * @two: second number input
- * Return: one + two
+ * @a: first number input
+ * @b: second number input
+ * Return: a + b
  */
 int add(int a, int b)
 {
 	int c;
+
 	c = (a + b);
 	return (c);
 }
