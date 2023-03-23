ac3e9e400f0d1512715e099ac1d96ee8c92b7950
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-23 14:19:33 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers
+ * @a : variable 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a = '2')
+		{
+			continue;
+		}
+		else if (a = '4')
+		{
+			continue;
+		}
+		else
+		{
+		_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
