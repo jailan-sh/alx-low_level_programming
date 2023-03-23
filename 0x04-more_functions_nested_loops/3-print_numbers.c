e4b96dc1665be9a238a2f4f578968027165c377a
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-23 13:44:46 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * @a : variable
+ * @a++ : increment
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a >= '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
+
