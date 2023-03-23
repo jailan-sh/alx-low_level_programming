b7ca843c467e2fcf12d38b642eaa87b15fc75da7
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-23 12:58:25 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
  * _isupper - check the code for Holberton School students.
  *@c : variable
  * Return: 0 or 1 .
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-	
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
