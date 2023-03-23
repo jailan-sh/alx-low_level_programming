5844495902c0fb8a1adb7e7ecff55b8b626c8c6b
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-23 15:01:05 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,19 @@
  * @a : variable 0 to 9
  * Return: void
  */
-
 void print_most_numbers(void)
 {
-	int a;
-
-	for  (a = '0'; a <= '9'; a++)
-	{
-		if ((a == '2') || (a == '4')
-				{
-				continue;
-				_putchar(a + 48);
-				}
-				_putchar('\n');
-				}
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+else
+{
+_putchar (i + '0');
+}
+}
+_putchar('\n');
+}
