b8d4455cf66cf09f8c620c0d429336103a9b0118
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-24 15:07:43 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - coding
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n;
+
+n=612852475143;
+
+long int div=2, ans = 0, maxFact;
+   while(n!=0)
+   {
+	   if(n % div !=0)
+	      div = div + 1;
+      else 
+      {
+	      maxFact = n;
+	 n = n / div;
+	 if(n == 1)
+	 {
+		 printf("%d is the largest prime factor !",maxFact);
+			    ans = 1;
+	    break;
+	 }
+      }
+   }
