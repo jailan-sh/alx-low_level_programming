dbaf06b6f7e5ecc0c0b423ea30b08135cc132108
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-24 16:27:28 -0700

    last

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,84 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints the biggest prime factor of a number.
- *
- * Return: Always 0.
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
  */
 
 int main(void)
-{
-	long int number;
-
-	number = 612852475143;
-
-	if (isPrime(number) == 1)
 	{
-		printf("%ld\n", number);
-	}
-	else
+	long num = 612852475143;
+	long divisor = 2;
+	long larg_prim = 0;
+
+	while (num != 1)
 	{
-		printf("%ld\n", biggestFactor(number));
+		if (num % divisor == 0)
+		{
+			num = num / divisor;
+			larg_prim = divisor;
+		}
+		divisor += 1;
 	}
+	printf("%ld\n", larg_prim);
 	return (0);
-}
-/**
- * isPrime - analise if a number is prime or not
- * @n: number to check
- * Return: true if it is prime false if not
- */
-int isPrime(long int n)
-{
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		for (i = 2; i < n; i++)
-		{
-			if (n % i == 0)
-			{
-				return (0);
-			}
-		}
-	return (1);
-	}
-}
-/**
- * biggestFactor - returns the biggest prime factor of a number
- * @a: number to check
- * Return: biggest factor
- */
-long int biggestFactor(long int a)
-{
-	long int i, factor;
-
-	factor = a;
-	for (i = 2; i <= factor; i++)
-	{
-		if (isPrime(factor) == 1)
-		{
-			break;
-		}
-		else
-		{
-			if ((factor % i == 0) && (isPrime(i) == 1))
-			{
-				factor = factor / i;
-				continue;
-			}
-			else
-			{
-				factor = factor;
-			}
-		}
-	}
-	return (factor);
 }
