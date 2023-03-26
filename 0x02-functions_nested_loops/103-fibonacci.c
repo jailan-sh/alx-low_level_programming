6fd305238c7aaaf6234bb07a1914322b398ad04a
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-25 17:21:46 -0700

    task101

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - coding
+ * return: 0
+ */
+
+int main(void)
+{
+	int a, i, b, result;
+	unsigned long sum;
+
+	a = 0;
+	b = 1;
+
+	for (i = 0; i <= 35; i++)
+	{
+		result = a + b;
+		a = b;
+		b = result;
+		if ((result < 4000000) && (result % 2 == 0))
+		{
+			sum += result;
+				printf("%lu\n", sum);
+		}
+	}
+}
