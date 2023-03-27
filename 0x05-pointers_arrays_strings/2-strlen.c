231c6e0a3850adc057bc43704da647fc28182d3c
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-27 08:54:57 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen : a function that returns the length of a string.
+ * @s : pointer
+ * return: void
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s[i] != \0; i++)
+}
