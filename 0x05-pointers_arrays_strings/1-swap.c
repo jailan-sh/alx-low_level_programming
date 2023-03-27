257254340a37170a7208a99f915c19049b5d27de
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-27 08:29:34 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int : swaps the values of two integers
+ * @a : variables
+ * @b : variable
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
