b37efa31ef750fe5591644582b47e5585129002c
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-27 16:31:21 -0700

    task 4

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] >= '\0'; i++)
 	{
-		if ((i = 0) || (i % 2 == 0))
+		if  (i % 2 == 0)
 
 		_putchar(str[i] + 0);
+
+		else
+			continue;
+
 	}
 	_putchar('\n');
 }
