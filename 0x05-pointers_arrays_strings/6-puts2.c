d942b070dad7167faf05d80c5eb7e33734ed6cee
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-27 16:19:33 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  * puts2 -  prints every other character of a string
+  * @str : pointer
+  * return: 0;
+  */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i] + 2);
+	}
+	_putchar('\n');
+}
