1001e7853d834478447773fde8211d792dea17a8
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-27 18:00:15 -0700

    task6clear

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0 ; i < n ; i++)
 	{
-		if (i < (n - 1))
+		printf("%d", *(a + i));
+		if (i < n - 1)
 		{
-			printf ("%d, ", a[i]);
+			printf(", ");
 		}
 	}
-	printf ("\n");
+	printf("\n");
 }
 
