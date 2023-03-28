553c37d3201f9f9b6ebcedc136473a9bb9152825
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-28 07:58:55 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,26 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_array - print `n` elements of an array of integers
- * @a: int type array pointer
- * @n: int type integer
- * Description: Numbers must be separated by comma and space.
- * Numbers should be displayed in the same order they are stored in array.
- * You can only use _putchar to print.
+ * print_array - a function that prints n elements of an array of integers
+ *
+ * @a : pointer
+ * @n : variable
+ *
+ * return: void
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
+		printf ("%d", a[i]);
+		if (i < (n - 1))
+			printf (", ");
 	}
-	printf("\n");
+	printf ("\n");
 }
-
