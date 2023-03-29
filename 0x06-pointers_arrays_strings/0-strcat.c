ab288a27b6ec8719b7c7013b466c1ffe7f297506
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-29 13:52:04 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat - a function that concatenates two strings
- * @dest : pointer to string
- * @src : pointer to string
- * return: dest
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int destlen = 0;
+	int index = 0;
+	int dest_len = 0;
 
-	while (dest[i++])
-		destlen++;
-	for (i= 0; src[i]; i++)
-		dest[destlen++] = src[i];
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
