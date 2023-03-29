e3bcec65b6515d09b69b6f1321b6e9ccc5fc4daf
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-29 13:54:43 -0700

    task0

--- 0x06-pointers_arrays_strings/jily.c
+++ 0x06-pointers_arrays_strings/jily.c
@@ -9,12 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int destlen = 0;
+	int index = 0;
+	int dest_len = 0;
 
-	while (dest[i++])
-		destlen++;
-	for (i= 0; src[i]; i++)
-		dest[destlen++] = src[i];
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
