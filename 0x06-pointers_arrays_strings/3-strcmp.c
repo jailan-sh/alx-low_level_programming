c6de375ad42952b84381ab1906f9610117d0678b
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-29 17:56:27 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,12 @@
 /**
  * _strcmp - a function that compares two strings
  * @s1 : first string
- * @s2 : second string 
- *
+ * @s2 : second string
+ * * Return: If str1 < str2, the negative difference of
+ * the first unmatched characters.
+ * If s1 == s2, 0.
+ * If s1 > s2, the positive difference of
+ * the first unmatched characters.
  */
 
 int _strcmp(char *s1, char *s2)
@@ -15,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 	{
 	if (s1[i] != s2[i])
 	{
-		return (s1[i]- s2[i]);
+		return (s1[i] - s2[i]);
 	}
 	}
 		return (0);
