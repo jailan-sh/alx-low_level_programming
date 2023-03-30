2b69abafce0c4bb86bc8310044980981253564d6
jailan.sh <jailan.elsherbiny@gmail.com>
2023-03-30 10:16:06 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper -  changes all lowercase letters of a string to uppercase.
+ * @x : pointer to function
+ *
+ *Return: x
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+		{
+			x[i] = x[i] - 32;
+		}
+		i++;
+	}
+	return (x);
+}
