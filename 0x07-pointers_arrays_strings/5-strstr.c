22ab08aaa023966112b87ee92bcfd9c88b288058
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-04 15:24:28 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,11 +11,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0;
+
 	while (*haystack)
 	{
-		if (*haystack == *needle)
-			return (haystack);
-				haystack++;
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+
+		}
+		haystack++;
 	}
 	return ('\0');
 }
