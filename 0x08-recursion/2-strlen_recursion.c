71c95011d4991c7277116d7d3bc1a3af1a3d5b0f
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-05 18:08:25 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - function that returns the length of a string
+ * @s : string
+ * Return: void
  */
 
 int _strlen_recursion(char *s)
@@ -12,5 +15,5 @@ int _strlen_recursion(char *s)
 		i++;
 		i += _strlen_recursion(s + 1);
 	}
-	return(i);
+	return (i);
 }
