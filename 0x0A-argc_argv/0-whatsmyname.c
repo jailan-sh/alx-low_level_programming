ffd998ed300b6c3c2383f77773e68d7ed1031177
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-07 18:44:35 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - coding
+ * Return: 0
+ * @argc : number of commend line
+ * @argv : char of arry of command line
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc *0]);
+	return (0);
+}	
