2fb5f267132e994fe51d955c6f935fa3ca3f7e0a
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-10 18:01:48 -0700

    task101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,8 +5,8 @@
  * wildcmp - a function that compares two strings
  * @s1 : first string
  * @s2 : second string
- * Return :  returns 1 if the strings can be considered identical
- * otherwise return 0
+ *
+ * Return: 1 if the strings can be considered identical otherwise 0
  */
 
 int wildcmp(char *s1, char *s2)
@@ -17,28 +17,28 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s1 == *s2)
 	{
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s2 == '*')
 	{
 		while (*(s2 + 1) == '*')
 		{
 			s2++;
-		}
 		if (*(s2 + 1) == '\0')
 		{
 			return (1);
 		}
+		}
+
 		while (*s1 != '\0')
 		{
-			if (wildcmp (s1, s2 + 1))
+			if (wildcmp(s1, s2 + 1))
 					{
 					return (1);
 					}
 					s1++;
 					}
-					}
-					return (0);
-					}
-
+	}
+					return (0);					}
+}
 
