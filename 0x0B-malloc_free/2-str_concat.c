9d0fa256dc0bb8ecab93fd2c9c5ac77f5fca7419
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-11 18:07:37 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,31 +13,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
+		int l1, l2, i, j;
 	char *new;
-	unsigned int l1 = strlen(s1);
-	unsigned int l2 = strlen(s2);
-	unsigned int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = " ";
-	}
 	if (s2 == NULL)
-	{
 		s2 = " ";
-	}
+
+	l1 = 0, l2 = 0;
+	while (*(s1 + l1))
+		l1++;
+	while (*(s2 + l2))
+		l2++;
+
 	new = malloc(sizeof(char) * (l1 + l2 + 1));
-			if (new == NULL)
-			{
-			return (NULL);
-			}
-			for (i = 0; i < l1; i++)
-			{
-				new[i] = s1[i];
-			}
-			for (i = 0, j = l1; i <= l2; j++, i++)
-			{
-				new[j] = s2[i];
-			}
-			return (new);
+
+	if (new == 0)
+		return (0);
+
+	for (i = 0; i < l1; i++)
+		*(new + i) = *(s1 + i);
+
+	for (i = 0, j = l1; i <= l2; j++, i++)
+		*(new+ j) = *(s2 + i);
+
+	return (new);
 }
