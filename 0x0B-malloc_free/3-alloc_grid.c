bd9b0d8c4b2b3e6737cd10edab6a0b9aa3ca3a74
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-12 07:19:36 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i <= height; i++)
 	{
-		grid[i] = valloc(width * sizeof(int *));
+		grid[i] = calloc(width * sizeof(int *));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
