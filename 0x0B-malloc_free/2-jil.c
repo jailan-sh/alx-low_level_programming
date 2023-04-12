053293e38942c44b4d6a6cc90814854834d73663
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-12 14:42:55 -0700

    task4

--- 0x0B-malloc_free/2-jil.c
+++ 0x0B-malloc_free/2-jil.c
@@ -19,11 +19,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 	new = malloc(l1 + l2 + 1);
 			if (new == NULL)
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * free_grid - frees the assigned memory of the matrix
- * @grid: the matrix
- * @height: number of rows.
+ * free_grid - frees a 2 dimensional grid previously
+ * created by your alloc_grid function
+ * @grid : 2D array
+ * @height : rows
  *
- * Return: pointer to the matrix.
+ * Return: void
  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-		free(*(grid + i));
+	{
+		free(grid[i]);
+	}
 	free(grid);
 }
+
