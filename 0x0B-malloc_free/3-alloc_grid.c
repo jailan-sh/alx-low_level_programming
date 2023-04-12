1a6c0a07ae11d9a85de09a21434a436344e02b9d
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-12 07:38:45 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,8 +4,8 @@
 /**
  * alloc_grid - function that returns a pointer to a
  * 2 dimensional array of integers.
- * @width : first array
- * @height : 2nd array
+ * @width : columns
+ * @height : rows
  *
  * Return: NULL on failure or pointer to 2D array
  */
@@ -27,16 +27,23 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i <= height; i++)
 	{
-		grid[i] = calloc(width * sizeof(int *));
+		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
-						}
-						free(grid);
-						return (NULL);
-						}
-						}
-						return (grid);
-						}
+			}
+				free(grid);
+				return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
