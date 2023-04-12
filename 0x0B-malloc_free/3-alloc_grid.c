7bffd27fa7da34cf1ea9f6a5c6d181133a018227
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-12 07:45:24 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,16 +34,14 @@ int **alloc_grid(int width, int height)
 			{
 				free(grid[j]);
 			}
-				free(grid);
+				free(grid[i]);
 				return (NULL);
 		}
 	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-		{
 			grid[i][j] = 0;
-		}
 	}
 	return (grid);
 }
