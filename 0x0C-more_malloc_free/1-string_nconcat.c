1722ce471b950992fe6b65c73e9524495f2c6328
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-14 06:05:27 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,38 +9,29 @@
  *
  * Return: Pointer to allocated memory of s1 + nbytes of s2
  */
+
+#include <string.h>
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j;
-	char *s;
-	char *nul = "";
+	char *new;
+	unsigned int i, j;
 
 	if (s1 == NULL)
-		s1 = nul;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = nul;
+	s2 = "";
 
-	l1 = 0, l2 = 0;
-	while (*(s1 + l1))
-		l1++;
-	while (*(s2 + l2))
-		l2++;
+	new = malloc(strlen(s1) + n + 1);
+	if (new == NULL)
+		return (NULL);
+	strcpy(new, s1);
 
-	if (n < l2)
-		l2 = n;
-
-	s = malloc(sizeof(char) * (l1 + l2 + 1));
-
-	if (s == 0)
-		return (0);
-
-	for (i = 0; i < l1; i++)
-		*(s + i) = *(s1 + i);
-
-	for (i = 0, j = l1; i < l2; j++, i++)
-		*(s + j) = *(s2 + i);
-
-	*(s + j) = '\0';
-
-	return (s);
+	for (i = 0, j = strlen(s1); i < n && s2[i] != '\0'; i++, j++)
+	{
+		new[j] = s2[i];
+	}
+	new[j] = '\0';
+	return (new);
+	free(new);
 }
