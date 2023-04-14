5a44553a09c2d105f4e05bb27762804d13cad755
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-14 13:37:49 -0700

    task3

--- 0x0C-more_malloc_free/3-jil.c
+++ 0x0C-more_malloc_free/3-jil.c
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 
 int *array_range(int min, int max)
+{
+	int *all;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	all = malloc((max - min + 1) * sizeof(int));
+	if (all == NULL)
+		return (NULL);
+	for (i = 0; i <= max; i++)
+	{
+		all[i] = min + i;
+	}
+	return (all);
+}
