ae19a0544b8ec8867c08f6ecbca64cbe85bc37fc
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-17 14:45:58 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_h_
+#define _DOG_h_
+
+/**
+ * struct dog -  Define a new type
+ * @name : char
+ * @age : float
+ * @owner : char
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+#endif
