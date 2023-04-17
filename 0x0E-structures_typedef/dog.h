e9376bb40ab6f77eba3c6798de6d27f33a570915
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-17 16:16:41 -0700

    task4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,18 @@
  * @owner : name of the owner
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
