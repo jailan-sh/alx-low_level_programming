ecbb21aee2e8b2940fe5a66c801bf57f7c4c0444
jailan.sh <jailan.elsherbiny@gmail.com>
2023-04-18 07:54:23 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,38 +5,23 @@
  * dog *d : struct pointer to dog
  * @d : pointer to struct
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
-	if (d != NULL)
-	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-	if (d->age == 0)
-	{
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
 		printf("Age: (nil)\n");
-	}
 	else
-	{
 		printf("Age: %f\n", d->age);
-	}
+
 	if (d->owner == NULL)
-	{
 		printf("Owner: (nil)\n");
-	}
 	else
-	{
 		printf("Owner: %s\n", d->owner);
-	}
-	}
 }
