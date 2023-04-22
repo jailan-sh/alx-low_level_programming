895bc996eb37066df43b6396f1eef3fb40b11981
jailan_sh <jilan.elsherbiny@gmail.com>
2023-04-22 11:10:44 -0700

    task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,24 +9,20 @@
  *
  * Return: No return
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list nums;
 
-	if (n == 0)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(nums, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));
-	if (separator == 0 && i < n - 1)
-		printf("%s", separator);
-}
-	printf("\n");
-	va_end(nums);
+		if (separato != NULL && i != (n - 1))
+			printf("%s",separator);
+	}
+				printf("\n");
+				va_end(nums);
 }
