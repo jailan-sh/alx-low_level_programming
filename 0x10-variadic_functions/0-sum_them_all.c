5a0a37859e8e0c1c91559571c103477cdbc4c9eb
jailan_sh <jilan.elsherbiny@gmail.com>
2023-04-22 11:03:35 -0700

    task 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0, i;
-	va_list list;
+	va_list args;
 
 	if (n == 0)
 		return (0);
-	va_start(list, n);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
+		sum += va_arg(args, unsigned int);
 
-	va_end(list);
+	va_end(args);
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,20 +12,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list list;
+	va_list nums;
 
 	if (n == 0)
 	{
 		printf("\n");
 		return;
 	}
-	if (separator == 0)
-		separator = "";
+	va_start(nums, n);
 
-	va_start(list, n);
-	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(list, int), separator);
-	printf("%d\n", va_arg(list, int));
-
-	va_end(list);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+	if (separator == 0 && i < n - 1)
+		printf("%s", separator);
+}
+	printf("\n");
+	va_end(nums);
 }
