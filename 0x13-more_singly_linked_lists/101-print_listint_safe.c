79f3de77c5799e9e9f30968966675008a19763f9
jailan-sh <jilan.elsherbiny@gmail.com>
2023-05-02 15:12:24 -0700

    task 102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,38 +1,44 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_listint_safe -  prints a listint_t linked list.
- * @head : pointer to linked list
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: A pointer to the head node of the list
  *
- * Return: the number of nodes in the list
+ * Return: The number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *prev;
-	unsigned int count = 0;
+    const listint_t *slow = head;
+    const listint_t *fast = head;
+    size_t count = 0;
+    int loop_detected = 0;
 
-	current = head;
-	if (current == 0)
-		return (0);
+    while (slow != NULL && fast != NULL && fast->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        count++;
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
+            loop_detected = 1;
+            break;
+        }
+    }
 
-	while (current != 0)
-	{
-		prev = current;
-		current = current->next;
+    if (!loop_detected)
+    {
+        while (head != NULL)
+        {
+            printf("[%p] %d\n", (void *)head, head->n);
+            count++;
+            head = head->next;
+        }
+    }
 
-		count++;
-
-		printf("[%p] %d\n", (void *)prev, prev->n);
-
-		if (prev <= current)
-		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
-		}
-	}
-
-	return (count);
+    return count;
 }
