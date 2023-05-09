77180967435ef3a4c8264c6faf9568689e98a529
jailan-sh <jilan.elsherbiny@gmail.com>
2023-05-08 17:12:34 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (len)
+	{
 	w = write(fd, text_content, len);
 	if (w == -1)
 	{
 		return (-1);
+	}
 	}
 	close(fd);
 	return (1);
