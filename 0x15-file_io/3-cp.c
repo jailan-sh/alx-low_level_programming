350027bc42f75502bbaab9fb96b06f47697dea79
jailan-sh <jilan.elsherbiny@gmail.com>
2023-05-10 15:04:05 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	outp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC , 0664);
+	outp = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (outp == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
