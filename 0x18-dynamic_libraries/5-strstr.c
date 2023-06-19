baff83498911cd7b83b97a44ddded4465531328c
jilan.elsherbiny <jilan.elsherbiny@gmail.com>
2023-06-19 09:59:53 -0700

    task0

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * needle in the string haystack
+ * @haystack : source string
+ * @needle: searching string
+ * Return: If a set is matched - a pointer to the locatesin haystring.
+ * If no set is matched - NULL.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+
+		}
+		haystack++;
+	}
+	return ('\0');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @num: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		num = num * (-1);
+			return (num);
+}
