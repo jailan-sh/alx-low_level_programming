9e23e217adeecbafe460bf9b4d4b7b637b4e2dfc
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-06 15:21:42 -0700

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size : size of hash table
+ *
+ * Return: hash_table or NULL if something goes wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	new_table->size = size;
+	return (new_table);
+}
