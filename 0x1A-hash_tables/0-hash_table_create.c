26071828d1938f4f9057838f240b531d3ec6f920
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-06 15:36:07 -0700

    1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - function that creates a hash table.
- * @size : size of hash table
+ * @size : size of array hash table
  *
  * Return: hash_table or NULL if something goes wrong
  */
@@ -10,10 +10,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_table_t *new_table;
+	hash_table_t *hash_table;
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
@@ -21,8 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = calloc(new_table->size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
-		free(new_table);
+		free(hash_table);
 		return (NULL);
 	}
-	return (new_table);
+	return (hash_table);
 }
