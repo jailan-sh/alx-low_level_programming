7be4e0645c3db7b4e3182a95652c7fd993ba6eaa
jailan-sh <jilan.elsherbiny@gmail.com>
2023-07-07 09:42:27 -0700

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * void hash_table_delete - function that deletes a hash table.
+ * @ht : hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *item, *ptr;
+	unsigned long int i;
+
+	if (ht ==NULL)
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+		while (item)
+		{
+			ptr = item->next;
+			free(item->key);
+			free(item->value);
+			free(item);
+			item = ptr;
+		}
+	}
+	free(ptr);
+	free(ht->array);
+	free(ht);
+}
