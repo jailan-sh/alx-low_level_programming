6426383377d457abb4d2a5164586e2c3fe0a8958
jailan-sh <jilan.elsherbiny@gmail.com>
2023-09-10 09:55:54 -0700

    edit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ find primeter of island  """
 
+
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid
     grid is a list of list of integers:
@@ -9,19 +10,19 @@ def island_perimeter(grid):
     One cell is a square with side length 1
     """
 
-    m = len(grid)
-    n = len(grid[0])
+    col = len(grid)
+    row = len(grid[0])
 
     islands = 0
     neighbors = 0
 
-    for i in range(m):
-        for j in range(n):
+    for i in range(col):
+        for j in range(row):
             if grid[i][j] == 1:
                 islands += 1
-                if i + 1 < m and grid[i + 1][j] == 1:
+                if i + 1 < col and grid[i + 1][j] == 1:
                     neighbors += 1
-                if j + 1 < n and grid[i][j + 1] == 1:
+                if j + 1 < row and grid[i][j + 1] == 1:
                     neighbors += 1
 
     return islands * 4 - neighbors * 2
