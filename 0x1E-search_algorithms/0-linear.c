49797651a0b6f54dfeadcd57e442f8e32e854658
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-11 01:24:10 +0300

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ * linear_search - function to find certain value
+ * @array: data to be searched
+ * @size: size of data
+ * @value: what we are looking for
+ * Return: value if present otherwise -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	long unsigned int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		}
+	}
+	return (-1);
+}
