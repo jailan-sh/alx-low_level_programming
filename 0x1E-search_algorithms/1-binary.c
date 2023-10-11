1b1817ec163c48a3f11c61aed98302868225019c
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-11 20:35:57 +0300

    O

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,39 +10,29 @@
 int binary_search(int *array, size_t size, int value)
 {
 	unsigned long int r = size - 1;
-	unsigned long int l, i = 0;
-	unsigned long int mid = 0;
+	unsigned long int l = 0;
+	unsigned long int mid, i;
 
 	if (array == NULL)
 	{
 	return (-1);
 	}
-	while (l < r)
+	while (l <= r)
 	{
-		mid = l + r / 2;
-
+		mid = l + (r - l) / 2;
+		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d", array[r]);
+		printf("\n");
 		if (array[mid] == value)
-		{
 			return (mid);
-		}
 		else if (value > array[mid])
-		{
-			printf("Searching in array:");
-			for (i = 0; i < r; i++)
-			{
-				printf("%d, \n",array[i]);
-			}
 			l = mid + 1;
-		}
 		else
-		{
-			printf("Searching in array: ");
-			for (i = 0; i < r; i++)
-			{
-				printf("%d, \n",array[i]);
-			}
 			r = mid - 1;
-		}
 	}
 	return (-1);
 }
